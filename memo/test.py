743bb9381fbc9dde80450f2a39d43b0056ba3932
송현광 <songhyeongwang@songhyeongwangs-MacBook-Pro.local>
2025-01-21 23:05:47 +0900

    max()

--- memo/test.py
+++ memo/test.py
@@ -1,25 +1,13 @@
 n, m = map(int, input().split())
-second_line = list(map(int, input().split()))
-arr = []
-arr2 = []
-second_line.sort()
+arr = list(map(int, input().split()))
+max_num = 0
 
 for i in range(n) :
-    for j in range(n-1) :
-        for k in range(n-2) :
-            arr.append(second_line[i]+second_line[j+1]+second_line[k+2])
-
-for i in range(len(arr)) :
-     arr2.append(m - arr[i])
-     print(arr2)
-
-arr2.sort()
-
-for i in range(len(arr2)) :
-    if 0 == arr2[i] :
-        print(m)
-        break
-    if arr2[i] > 0 :
-        print(m+arr2[i])
-        break
- 
+    for j in range(i+1, n) :
+        for k in range(j+1, n) :
+            if arr[i] + arr[j] + arr[k] > m :
+                continue
+            else :
+                max_num = max(max_num, arr[i] + arr[j] + arr[k])
+            
+print(max_num)
