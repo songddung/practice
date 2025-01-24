eb5b320fc9036e816fcc778885506db5da3b40ac
ggalong <shk9521@naver.com>
2025-01-24 16:10:26 +0900

    data_structure

---
+++ 08-data-structure/03-list.py
@@ -0,0 +1,63 @@
+# append
+my_list = [1, 2, 3]
+
+print(my_list)  # [1, 2, 3, 4]
+print()  #
+
+# extend
+my_list = [1, 2, 3]
+
+print(my_list)  # [1, 2, 3, 4, 5, 6]
+
+# append와의 비교
+
+
+# insert
+my_list = [1, 2, 3]
+
+print(my_list)  # [1, 5, 2, 3]
+
+# remove
+my_list = [1, 2, 3, 2, 2, 2]
+
+print(my_list)  # [1, 3, 2, 2, 2]
+
+# pop
+my_list = [1, 2, 3, 4, 5]
+item1 = None
+item2 = None
+
+print(item1)  #
+print(item2)  #
+print(my_list)  #
+
+
+# clear
+my_list = [1, 2, 3]
+
+print(my_list)  # []
+
+# index
+my_list = [1, 2, 3]
+index = None
+print(index)  # 1
+
+# count
+my_list = [1, 2, 2, 3, 3, 3]
+counting_number = None
+print(counting_number)  # 3
+
+# reverse
+my_list = [1, 3, 2, 8, 1, 9]
+
+print()  #
+print(my_list)  #
+
+# sort
+my_list = [3, 2, 100, 1]
+
+print(my_list)  # [1, 2, 3, 100]
+
+# sort(내림차순 정렬)
+
+print(my_list)  # [100, 3, 2, 1]
